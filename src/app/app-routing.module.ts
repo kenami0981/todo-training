c9afabf35f0d81574593bac071340ef18a3a5cb5
kenami0981 <amelia.janek.aj@gmail.com>
2022-03-07 19:24:37 +0100

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsPageModule } from './pages/about-us.page-module';
 import { HomePage } from './pages/home.page';
 import { HomePageModule } from './pages/home.page-module';
+import { KontaktPageModule } from './pages/kontakt.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'about-us', 
+        loadChildren: () => AboutUsPageModule
+      },
+  { 
+        path: 'home', 
+        loadChildren: () => HomePageModule
+      },
+  { 
+        path: 'kontakt', 
+        loadChildren: () => KontaktPageModule
+      }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), HomePageModule, AboutUsPageModule],
+  imports: [RouterModule.forRoot(routes), HomePageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/kontakt.page.ts
@@ -0,0 +1,5 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({ selector: 'lib-kontakt-page', templateUrl: './kontakt.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+export class KontaktPage {
+}
