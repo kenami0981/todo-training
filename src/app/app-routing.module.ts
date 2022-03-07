18f6b56898eeb1723ae3ca38091c7025d325512f
kenami0981 <amelia.janek.aj@gmail.com>
2022-03-07 18:46:31 +0100

    second-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageModule } from './pages/about-us.page-module';
+import { HomePage } from './pages/home.page';
+import { HomePageModule } from './pages/home.page-module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomePageModule, AboutUsPageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
