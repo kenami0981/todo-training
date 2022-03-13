fa238852884d27179a31b79f710ea8e9384ab229
kenami0981 <amelia.janek.aj@gmail.com>
2022-03-13 19:37:41 +0100

    L2P2

--- projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
+++ projects/messages/src/lib/adapters/primary/ui/contact-form.component.ts
@@ -1,8 +1,11 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { AlertPromise } from 'selenium-webdriver';
 
 @Component({ selector: 'lib-contact-form', templateUrl: './contact-form.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class ContactFormComponent {
   readonly messageForm: FormGroup = new FormGroup({email: new FormControl(), text: new FormControl()});
-  onFormSubmited(messageForm: FormGroup): void {console.log(messageForm.getRawValue());}
+  onFormSubmited(messageForm: FormGroup){
+    alert("email: " + messageForm.get("email").value +" text: "+  messageForm.get("text").value)
+  }
 }
